44db235b86dd9853d7a7eb51dacd9dec14d04769
JoHyungJun <jhj8371@naver.com>
2023-10-17 20:29:26 +0900

    project init

---
+++ src/main/java/zeobase/ZB_technical/challenges/ZbTechnicalChallengesApplication.java
@@ -0,0 +1,13 @@
+package zeobase.ZB_technical.challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbTechnicalChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbTechnicalChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zeobase/ZB_technical/challenges/ZbTechnicalChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package zeobase.ZB_technical.challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbTechnicalChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
