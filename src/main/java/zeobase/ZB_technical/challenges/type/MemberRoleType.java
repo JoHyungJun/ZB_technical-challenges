23cf54f705a1755c99b4883a484d3fa70980ae1b
JoHyungJun <jhj8371@naver.com>
2023-10-19 00:20:06 +0900

    [#1] Entity type 추가 및 변경

---
+++ src/main/java/zeobase/ZB_technical/challenges/type/MemberRoleType.java
@@ -0,0 +1,7 @@
+package zeobase.ZB_technical.challenges.type;
+
+public enum MemberRoleType {
+
+    MEMBER,
+    STORE_OWNER
+}
---
+++ src/main/java/zeobase/ZB_technical/challenges/type/ReservationAcceptedType.java
@@ -0,0 +1,8 @@
+package zeobase.ZB_technical.challenges.type;
+
+public enum ReservationAcceptedType {
+
+    RESERVATION_ACCEPTED,
+    RESERVATION_REJECTED,
+    RESERVATION_WAITING
+}
