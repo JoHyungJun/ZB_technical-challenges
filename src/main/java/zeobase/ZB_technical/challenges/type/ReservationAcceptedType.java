b2d4f3b3c65a4e4d73720161c3a366dae69236ae
JoHyungJun <jhj8371@naver.com>
2023-10-24 00:26:32 +0900

    [#1] Entity field 추가 및 변경

--- src/main/java/zeobase/ZB_technical/challenges/type/ReservationAcceptedType.java
+++ src/main/java/zeobase/ZB_technical/challenges/type/ReservationAcceptedType.java
@@ -2,7 +2,7 @@ package zeobase.ZB_technical.challenges.type;
 
 public enum ReservationAcceptedType {
 
-    RESERVATION_ACCEPTED,
-    RESERVATION_REJECTED,
-    RESERVATION_WAITING
+    ACCEPTED,
+    REJECTED,
+    WAITING
 }
