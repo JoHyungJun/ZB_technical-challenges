ff05a2f2264565c5236c68509920188dd7eedda0
JoHyungJun <jhj8371@naver.com>
2023-12-07 01:55:27 +0900

    [#16] 리뷰 숨기기 (soft delete) 기능 추가 - 리뷰가 달린 해당 매장의 점주만 이용 가능

---
+++ src/main/java/zeobase/zbtechnical/challenges/dto/review/response/ReviewHideResponse.java
@@ -0,0 +1,15 @@
+package zeobase.zbtechnical.challenges.dto.review.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewHideResponse {
+
+    private Long reviewId;
+}
